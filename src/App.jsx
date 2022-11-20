7db7f7bce423afb02ab678210353a5a53320a6ae
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-11-19 21:37:27 -0800

    testing design with using svg image files instead of manually typing the code

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import LocationDetails from "./components/LocationDetails";
 
 function App() {
-    return <LocationDetails />;
+  return (
+    <div>
+      <img src="assets/layouts/home-layout.svg" className="home-layout" />
+      <img src="assets/layouts/menu-layout.svg" />
+    </div>
+  );
+  // return <LocationDetails />;
 }
 
 export default App;
