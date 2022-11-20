8f9b9b1e7c279cd51da39ddedd7367e747881239
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-11-19 16:06:14 -0800

    added fonts and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
-
+import ramen1 from "./assets/ramen/niboshi-ramen.png";
 function App() {
-  return <div>Ichitaru Ramen</div>;
+  return (
+    <div>
+      Ichitaru Ramen <img src={ramen1} />
+    </div>
+  );
 }
 
 export default App;
