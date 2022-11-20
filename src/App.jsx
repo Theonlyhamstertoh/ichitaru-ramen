ee8824aa005552e1bdf44cbf4706ba0b104abc38
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-11-19 16:21:33 -0800

    move images to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import { useState } from "react";
 import LocationDetails from "./components/LocationDetails";
 
-import ramen1 from "./assets/ramen/niboshi-ramen.png";
 function App() {
-    return (
-        <div>
-            Ichitaru Ramen <img src={ramen1} />
-        </div>
-    );
-    return <LocationDetails />;
+  return <LocationDetails />;
 }
 
 export default App;
