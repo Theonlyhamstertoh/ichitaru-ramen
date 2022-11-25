23740087890154505b91c430d3d62f7d0b6cffde
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-11-25 08:06:56 -0800

    set background pattern in body to repeat on y

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import LocationDetails from "./components/LocationDetails";
 function App() {
   return (
     <div>
-      <img src="assets/layouts/home-layout.svg" className="home-layout" />
-      <img src="assets/layouts/menu-layout.svg" />
+      <Background />
+      {/* <img src="assets/layouts/home-layout.svg" className="home-layout" /> */}
+      {/* <img src="assets/layouts/menu-layout.svg" /> */}
+      {/* <img src="assets/layouts/background.svg" className="background-pattern" /> */}
     </div>
   );
   // return <LocationDetails />;
 }
 
+function Background() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
 export default App;
