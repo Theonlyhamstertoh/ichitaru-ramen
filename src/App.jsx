fb63c7113fc4fc9c25f0fbddf63958c5014218b8
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-11-25 08:45:41 -0800

    completed basic home section

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import LocationDetails from "./components/LocationDetails";
 function App() {
   return (
     <div>
+      <HomeLayout />
       <Background />
-      {/* <img src="assets/layouts/home-layout.svg" className="home-layout" /> */}
       {/* <img src="assets/layouts/menu-layout.svg" /> */}
       {/* <img src="assets/layouts/background.svg" className="background-pattern" /> */}
     </div>
@@ -16,7 +16,21 @@ function App() {
 function Background() {
   return (
     <div>
-      <div></div>
+      <div className=""></div>
+    </div>
+  );
+}
+function HomeLayout() {
+  return (
+    <div className="brown-background">
+      <img src="assets/layouts/home-layout-mountains.png" className="home-layout-mountain" />
+      <div className="flex-center ">
+        <img className="ichitaru-logo" src="/assets/icons/ichitaru-logo.jpg" />
+        <img
+          className="online-order-button v-margin-10"
+          src="/assets/icons/online-order-button.svg"
+        />
+      </div>
     </div>
   );
 }
