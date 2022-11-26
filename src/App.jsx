fa5d4c8cc736ecbbc842bb21c7552bdb1d28d5e6
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-11-25 21:12:25 -0800

    complete basic mobile layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import Location from "./components/Location";
 import { ramens } from "./ramens";
 function App() {
+  const isMobile = useResize();
   return (
     <div>
-      <HomeLayout />
+      <HomeLayout isMobile={isMobile} />
 
       <Location />
       <Ramen />
-      {/* <Menu /> */}
+      <Menu isMobile={isMobile} />
       {/* <img src="assets/layouts/background.svg" className="background-pattern" /> */}
     </div>
   );
   // return <LocationDetails />;
 }
 
-function Menu() {
+function Menu({ isMobile }) {
   return (
     <div>
-      <img src="assets/layouts/menu-layout.svg" className="menu-layout" />
+      <img
+        src={`assets/layouts/menu-layout${isMobile ? "-mobile" : ""}.svg`}
+        className="menu-layout"
+      />
     </div>
   );
 }
@@ -32,7 +37,7 @@ function Ramen() {
       <h1 className="flex-center">Ramen</h1>
       <div className="ramen-slideshow">
         {ramens.map(({ src, name, id, isNew, className }) => (
-          <img key={id} className={`ramen ${className ?? ""}`} src={src} />
+          <img key={id} className="ramen" src={src} />
         ))}
       </div>
     </div>
@@ -45,18 +50,32 @@ function Background() {
     </div>
   );
 }
-function HomeLayout() {
+function HomeLayout({ isMobile }) {
   return (
-    <div className="brown-background">
-      <img src="assets/layouts/home-layout-mountains.png" className="home-layout-mountain" />
-      <div className="flex-center ">
-        <img className="ichitaru-logo" src="/assets/icons/ichitaru-logo.jpg" />
-        <img
-          className="online-order-button v-margin-10"
-          src="/assets/icons/online-order-button.svg"
-        />
-      </div>
+    <div className={`${isMobile ? "home-section" : "home-section"}`}>
+      {/* <img
+        className="online-order-button v-margin-10"
+        src="/assets/icons/online-order-button.svg"
+      /> */}
+      {/* <img className="home-background" src="assets/layouts/home-layout-desktop.svg" /> */}
     </div>
   );
 }
 export default App;
+
+function useResize() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+  const isMobile = width <= 768;
+
+  return isMobile;
+}
