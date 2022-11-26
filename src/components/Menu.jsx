c79786ad6e28b3e8099808b7b05678658e2672a5
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-11-26 13:01:32 -0800

    pull

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,7 +2,7 @@ export default function Menu({ isMobile }) {
   return (
     <div>
       <img
-        src={`assets/layouts/menu-layout${isMobile ? "-mobile" : ""}.svg`}
+        src={`assets/layouts/menu-layout${isMobile ? "-mobile.png" : ".svg"}`}
         className="menu-layout"
       />
     </div>
